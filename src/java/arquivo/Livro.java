4aa42222148ab24992aef56a6c077b3c6fd7f810
Jampierre Vieira Rocha <Jampierre@172.20.39.33>
2015-09-09 14:31:18 -0300

    commit de test

---
+++ src/java/arquivo/Livro.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arquivo;
+
+import javax.annotation.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author Jampierre
+ */
+@ManagedBean
+@SessionScoped
+public class Livro {
+    private int codigo;
+    private String titulo;
+    private String autor;
+
+    // não esqueça o construtor vazio
+    public Livro(){}
+
+    public Livro(int codigo, String titulo, String autor){
+        this.codigo = codigo;
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+}
